8eb0e22e61f9f8f62167ef7d043d43fa363a4b48
JoshnaMedisetty <107022821+JoshnaMedisetty@users.noreply.github.com>
2025-04-29 14:57:09 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,11 @@
 
+import os
+
+RANDOM_STATE = 42
+DATA_PATH = '../data/pima_diabetes.csv'
+RESULTS_DIR = '../results'
+FEATURE_NAMES = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
+                 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+
+os.makedirs(RESULTS_DIR, exist_ok=True)
+
